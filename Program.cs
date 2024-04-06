cc80bc39dd8adf3120c47738241fec2f6ccaa331
sebastiaojirmao <seba.irmao@gmail.com>
2024-04-06 12:20:01 -0300

    Resolve o exercicio Musica

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// intellisense
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+Console.Clear();
+Console.WriteLine("**Quem de Nós Dois**");
+Console.ResetColor();
+Console.WriteLine();
+Console.WriteLine("Eu e você \r\nNão é assim tão complicado\r\nNão é difícil perceber");
+Console.WriteLine();
+Console.WriteLine("Quem de nós dois\r\nVai dizer que é impossível\r\nO amor acontecer");
+Console.WriteLine();
+Console.WriteLine("Se eu disser que já nem sinto nada\r\nQue a estrada sem você é mais segura\r\nEu sei você vai rir da minha cara\r\nEu já conheço o teu sorriso, leio teu olhar\r\nTeu sorriso é só disfarce\r\nQue eu já nem preciso");
+Console.WriteLine();
+Console.WriteLine("Sinto dizer\r\nQue amo mesmo,\r\nTá ruim pra disfarçar");
+Console.WriteLine();
+Console.WriteLine("Entre nós dois\r\nNão cabe mais nenhum segredo\r\nAlém do que já combinamos");
+Console.WriteLine();
+Console.WriteLine("No vão das coisas que a gente disse\r\nNão cabe mais sermos somente amigos\r\nE quando eu falo que eu já nem quero\r\nA frase fica pelo avesso\r\nMeio na contra-mão\r\nE quando finjo que esqueço\r\nEu não esqueci nada");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("E cada vez que eu fujo, eu me aproximo mais\r\nE te perder de vista assim é ruim demais\r\nE é por isso que atravesso o teu futuro\r\nE faço das lembranças um lugar seguro");
+Console.WriteLine();
+Console.WriteLine("Não é que eu queira reviver nenhum passado\r\nNem revirar um sentimento revirado\r\nMas toda vez que eu procuro uma saída\r\nAcabo entrando sem querer na tua vida");
+Console.WriteLine();
+Console.ResetColor();
+
+Console.WriteLine("Eu procurei\r\nQualquer desculpa\r\nPra não te encarar");
+Console.WriteLine();
+Console.WriteLine("Para não dizer\r\nDe novo e sempre a mesma coisa\r\nFalar só por falar");
+Console.WriteLine();
+Console.WriteLine("Que eu já não tô nem aí pra essa conversa\r\nQue a história de nós dois não me interessa\r\nSe eu tento esconder meias verdades\r\nVocê conhece o meu sorriso\r\nLê no meu olhar\r\nMeu sorriso é só disfarce\r\nPorque eu já nem preciso");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("E cada vez que eu fujo, eu me aproximo mais\r\nE te perder de vista assim é ruim demais\r\nE é por isso que atravesso o teu futuro\r\nE faço das lembranças um lugar seguro");
+Console.WriteLine();
+Console.WriteLine("Não é que eu queira reviver nenhum passado\r\nNem revirar um sentimento revirado\r\nMas toda vez que eu procuro uma saída\r\nAcabo entrando sem querer na tua vida");
+Console.ResetColor();
+
